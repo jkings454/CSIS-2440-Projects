6bcc5d718138a2f0df5b1ec82fc2b5cc9c8b2786
Josh Nichols <josh.nichols.lives@gmail.com>
2016-10-18 19:39:12 -0600

    testing gitignore

--- skynet/skynet.js
+++ skynet/skynet.js
@@ -1,3 +1,11 @@
+/*
+  As well as w3 schools, I learned a lot from the book "Eloquent Javascript"
+  http://eloquentjavascript.net/
+  It's pretty good, and it's free to learn online.
+*/
+
+// I set up a fake unix terminal because reasons. The code for the guessing game is
+// in a method towards the bottom of the screen.
 var terminal = document.getElementById("faux-terminal");
 
 var homeDirectory = new Directory("~");
@@ -7,19 +15,26 @@ homeDirectory.addSubDirectory(testDirectory);
 
 var currentDirectory = homeDirectory;
 
+// A javascript object that maps various commands to functions.
 var commands = {
   "ls" : showCurrentDirectory,
   "cd" : changeDirectory,
   "help" : showHelp
 }
+guessingGame();
 
-var promptContent = document.createElement('div');
-promptContent.setAttribute('id', 'term-prompt');
-promptContent.innerHTML = "<label>user@skynet:" + currentDirectory.getPath() + "$</label> ";
-promptContent.innerHTML += "<input id='prompt' autofocus></input>";
-terminal.appendChild(promptContent);
-
+// This function begins the fake terminal emulation. It's called after the user finishes the guessing game.
+function beginTerminal(){
+  var promptContent = document.createElement('div');
+  promptContent.setAttribute('id', 'term-prompt');
+  promptContent.innerHTML = "<label>user@skynet:" + currentDirectory.getPath() + "$</label> ";
+  promptContent.innerHTML += "<input id='prompt' autofocus></input>";
+  terminal.appendChild(promptContent);
+  // This just makes it so the user can use enter to submit a guess.
+}
+// This is a javascript class describing a directory (folder).
 function Directory(name){
+  // Javascript: Making orphans since 1995.
   this.parent = null;
   this.subdirectories = [".", ".."];
   this.name = name;
@@ -40,20 +55,28 @@ function Directory(name){
   }
 }
 terminal.addEventListener("keydown", function(e) {
-  if (e.keyCode == 13) {
-    var value = document.getElementById("prompt").value;
-    try {
-      processCommand(value);
+    if (e.keyCode == 13) {
+      var value = document.getElementById("prompt").value;
+      // Some really fun exception handling.
+      try {
+        processCommand(value);
+      }
+      /*
+        Rant time:
+          Javascript has one exception class. And as far as I know, there's no distinction between
+          the different types of errors. So if litterally ANYTHING goes wrong, this will catch it, 
+          not just what I want it to catch.
+        Sorry about that.
+      */
+      catch (exception) {
+        var errormessage = document.createElement('p');
+        errormessage.innerHTML = "<span class='error'>No command: " + value + "</span>";
+        errormessage.innerHTML += "<p>Type 'help' for a list of commands.</p>";
+        outputPrompt(currentDirectory.getPath(), value, errormessage)
+      }
     }
-    catch (exception) {
-      var errormessage = document.createElement('p');
-      errormessage.innerHTML = "<span class='error'>No command: " + value + "</span>";
-      errormessage.innerHTML += "<p>Type 'help' for a list of commands.</p>";
-      outputPrompt(currentDirectory.getPath(), value, errormessage)
-    }
-
-  }
-});
+  });
+// This function lists everything in the current directory.
 function showCurrentDirectory(){
   var list = document.createElement("p");
   list.innerHTML = currentDirectory.subdirectories[0] + " ";
@@ -63,6 +86,7 @@ function showCurrentDirectory(){
   }
   return list;
 }
+// This sets the current directory.
 function changeDirectory(directory){
   var oldDir = currentDirectory.getPath();
   if (directory == "..") {
@@ -87,6 +111,8 @@ function changeDirectory(directory){
   return null;
 
 }
+// Processes the terminal input and acts accordingly using that nifty commands object
+// that I set up.
 function processCommand(value){
   var splitVal = value.split(" ");
   var result;
@@ -99,6 +125,7 @@ function processCommand(value){
   }
   outputPrompt(dirpath, value, result);
 }
+// basically, displays a prompt 
 function outputPrompt(dirpath, value, result){
   var p = document.createElement('p');
   p.innerText = "user@skynet:" + dirpath + "$ " + value;
@@ -113,11 +140,22 @@ function outputPrompt(dirpath, value, result){
   document.getElementById("prompt").value = "";
   document.getElementById("prompt").focus();
 }
+// Should be called if the user types "help"
+// displays a help message.
 function showHelp() {
   var helpmessage = document.createElement("p");
   helpmessage.innerHTML = "The following commands are available to you: ";
   Object.keys(commands).forEach(function(key) {
-    helpmessage.innerHTML += "<p class = context>" + key + "</p>";
+    helpmessage.innerHTML += "<p class = 'context'>" + key + "</p>";
   });
   return helpmessage;
+}
+function guessingGame() {
+  // This is mostly for flavor.
+  skyString =  "   _____ __ ____  ___   ______________ \n"
+  skyString += "  / ___// //_/\ \/ / | / / ____/_  __/\n";
+  skyString += "  \__ \\ / ,<    \  /  |/ / __/   / /   \n";
+  skyString += " ___/ / /| |   / / /|  / /___  / /    \n";
+  skyString += "/____/_/ |_|  /_/_/ |_/_____/ /_/     ";
+  terminal.innerHTML += "<pre>" + skyString + "</pre>";
 }
