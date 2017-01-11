582f757eea3b4c275a0a77aec81b30b3e04f0d34
Josh Nichols <josh.nichols.lives@gmail.com>
2017-01-11 13:41:33 -0700

    Removed unwanted items

--- CSIS2440/HelloWorld.php
+++ CSIS2440/HelloWorld.php
@@ -4,13 +4,15 @@
     <title>Hello World!</title>
     <link rel='stylesheet' type='text/css'
           href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'/>
-    <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
-    <script type='text/javascript' src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+    <script type='text/javascript'
+            src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+    <script type='text/javascript'
+            src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 <body>
 <div class='container'>
-
+    <a href = "/">Back to homepage.</a>
     <?php
         echo "<h1>Hello world</h1>";
         $greeting = "PHP is fun!\n";
